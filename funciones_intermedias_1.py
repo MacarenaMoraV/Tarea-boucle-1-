2ae9c055dd921406ca0733f64aca7e2c0882c92d
MacarenaMoraV <macarena.mora25@gmail.com>
2025-05-17 17:37:05 -0400

    Add files via upload
    #TAREA ACTUALIZAR VALORES DICCIONARIO Y LISTA

---
+++ funciones_intermedias_1.py
@@ -0,0 +1,22 @@
+
+#TAREA ACTUAIZAR VALORES DICCIONARIO Y LISTA
+#Ejercicio 1
+matriz = [[10, 15,20],[3,7,14]]
+matriz[1][0] = 6
+print(matriz)
+
+#Ejercicio 2
+cantantes = [{'Nombre': 'Ricky Martin', 'pais': 'Puerto Rico'},{'Nombre': 'Chayanne', 'pais': 'Puerto Rico'}]
+cantantes[0]['Nombre'] = 'Enrique Martin Morales'
+print(cantantes)
+
+#Ejercicio 3
+ciudades = [{'Mexico': ['Ciudad de Mexico', 'Guadalajara', 'Cancun'], 'Chile': ['Santiago', 'Concepcion', 'Viña del Mar']}]
+ciudades[0]['Mexico'][2] = 'Monterrey'
+print(ciudades)
+
+#Ejercicio 4
+coordenadas= [{'latitud': 8.2588997, 'longitud': -849399704}]
+coordenadas[0]['latitud'] = 9.9355431
+print(coordenadas)
+
